b2c25136cea983dd58fbce71254690eefadad406
Kaue-Dev <kauemassola900@gmail.com>
2024-11-26 14:26:38 -0300

    Commit 14

--- NewReleasePrice.java
+++ NewReleasePrice.java
@@ -8,4 +8,9 @@ public class NewReleasePrice extends Price {
   public double getCharge(int daysRented) {
     return daysRented * 3;
   }
+
+  @Override
+  public int getFrequentRenterPoints(int daysRented) {
+    return (daysRented > 1) ? 2 : 1; // 2 pontos para locações de mais de 1 dia
+  }
 }
