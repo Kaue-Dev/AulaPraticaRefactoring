dd5c580e3dc4d12d88e884c961a5e20f97488124
Kaue-Dev <kauemassola900@gmail.com>
2024-11-26 14:10:23 -0300

    Commit 11

--- Movie.java
+++ Movie.java
@@ -4,28 +4,40 @@ public class Movie {
   public static final int NEW_RELEASE = 1;
 
   private String _title;
-  private int _priceCode;
+  private Price _price;
 
   public Movie(String title, int priceCode) {
     _title = title;
-    _priceCode = priceCode;
-  }
-
-  public int getPriceCode() {
-    return _priceCode;
-  }
-
-  public void setPriceCode(int arg) {
-    _priceCode = arg;
+    setPriceCode(priceCode);
   }
 
   public String getTitle() {
     return _title;
   }
 
+  public int getPriceCode() {
+    return _price.getPriceCode();
+  }
+
+  public void setPriceCode(int priceCode) {
+    switch (priceCode) {
+      case REGULAR:
+        _price = new RegularPrice();
+        break;
+      case CHILDRENS:
+        _price = new ChildrensPrice();
+        break;
+      case NEW_RELEASE:
+        _price = new NewReleasePrice();
+        break;
+      default:
+        throw new IllegalArgumentException("Incorrect Price Code");
+    }
+  }
+
   public double getCharge(int daysRented) {
     double result = 0;
-    switch (_priceCode) {
+    switch (getPriceCode()) {
       case REGULAR:
         result += 2;
         if (daysRented > 2) {
@@ -45,11 +57,10 @@ public class Movie {
     return result;
   }
 
-  // Novo método que calcula os pontos de locação frequente
   public int getFrequentRenterPoints(int daysRented) {
-    if (_priceCode == NEW_RELEASE && daysRented > 1) {
-      return 2; // Bônus para novos lançamentos alugados por mais de 1 dia
+    if (getPriceCode() == NEW_RELEASE && daysRented > 1) {
+      return 2;
     }
-    return 1; // Padrão: 1 ponto por aluguel
+    return 1;
   }
 }
