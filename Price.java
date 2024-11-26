314d8b758c0445e4f41b37cc8490ee8b68bc1009
Kaue-Dev <kauemassola900@gmail.com>
2024-11-26 14:23:45 -0300

    Commit 13

--- Price.java
+++ Price.java
@@ -1,3 +1,7 @@
 public abstract class Price {
+  // Método abstrato para retornar o código de preço
   public abstract int getPriceCode();
+
+  // Método abstrato para calcular o valor da cobrança
+  public abstract double getCharge(int daysRented);
 }
